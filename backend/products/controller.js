9fb1553543c52a4de0281efb2a767285261fe877
DonRyu <alpharyu1031@gmail.com>
2023-03-27 13:39:42 -0700

    backend clean

---
+++ backend/products/controller.js
@@ -0,0 +1,68 @@
+const getProduct = (req, res) => {
+    const database = req.context.database;
+    const { page, query, queryType } = req.query;
+    const data = database.get(query, queryType);
+    const perPage = 10;
+    const start = (parseInt(page) - 1) * perPage;
+    const end = start + perPage;
+    const pageData = data.slice(start, end);
+    if (pageData) {
+      return res.status(200).send({
+        pageData: pageData,
+        currentPage: page,
+        totalPages: Math.ceil(data.length / perPage),
+        totalProduct: data.length,
+      });
+    } else {
+      return res.status(500).send({ msg: "error" });
+    }
+  };
+  
+  const getProductData = (req, res) => {
+    const productNumber = req.params.id;
+    const database = req.context.database;
+    const data = database.getByProductNumber(productNumber);
+    if (data) {
+      res.status(200).send(data);
+    } else {
+      return res.status(500).send({ msg: "error" });
+    }
+  };
+  
+  const postProduct = (req, res) => {
+    const database = req.context.database;
+    const data = database.post(req.body);
+    if (data) {
+      return res.status(200).send({ msg: "Successfully Add" });
+    } else {
+      return res.status(500).send({ msg: "error" });
+    }
+  };
+  
+  const putProduct = (req, res) => {
+    const database = req.context.database;
+    const data = database.put(req.body.productNumber, req.body.values);
+    if (data) {
+      return res.status(200).send({ msg: "Successfully Edit" });
+    } else {
+      return res.status(500).send({ msg: "error" });
+    }
+  };
+  
+  const deleteProduct = (req, res) => {
+    const database = req.context.database;
+    const data = database.delete(req.body.productNumber);
+    if (data) {
+      return res.status(200).send({ msg: "Successfully Delete" });
+    } else {
+      return res.status(500).send({ msg: "error" });
+    }
+  };
+
+  module.exports = {
+    getProduct,
+    getProductData,
+    postProduct,
+    putProduct,
+    deleteProduct
+  }
