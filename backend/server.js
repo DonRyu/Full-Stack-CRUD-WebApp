ca281c280907addbd07bcfbe9d5d83d99350c517
DonRyu <alpharyu1031@gmail.com>
2023-03-26 22:31:29 -0700

    swagger 예시 추가

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,16 @@ const port = 3000;
 const Database = require("./database/database");
 const database = new Database();
 const products = require('./products/routes')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((req, _, next) => {
   req.context = { database };
   next();
